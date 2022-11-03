169a479ccfdde1d7c5f663fa9c5d7b88b46235c9
hieu22012003 <101488865+hieu22012003@users.noreply.github.com>
2022-11-03 13:10:41 +0700

    Update and rename demoComplex.java to ComplexDemo.java

--- thi/src/main/java/ComplexDemo.java
+++ thi/src/main/java/ComplexDemo.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Admin
- */
 import java.util.*;
-public class demoComplex {
+public class ComplexDemo {
     public static void main(String[] args)
 	{
 		boolean debug = false;
